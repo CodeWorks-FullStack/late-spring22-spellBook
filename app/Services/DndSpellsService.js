69e8b61533b4c5fa3b5522ebccfcea89efafdd8b
Mick Shannahan <MickShanny@gmail.com>
2022-06-01 17:17:28 -0600

    no map s => s

--- app/Services/DndSpellsService.js
+++ app/Services/DndSpellsService.js
@@ -11,7 +11,8 @@ class DndSpellsService{
   async getDndSpells() {
     const res = await dndApi.get()
     console.log('getDndSpells', res.data);
-    ProxyState.dndSpells = res.data.results.map(s => s)
+    // NOTE if you saw .map(s => s) here before that's cause I forgot to get rid of the map after we decided these were not 'Spells'
+    ProxyState.dndSpells = res.data.results
   }
   
   async getSpellDetails(index) {
